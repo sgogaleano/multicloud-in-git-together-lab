5dc810cf013a50f55af1e56d14eb3d6407e4de4e
Mauricio Ospina Vergara <55215654+mauosp@users.noreply.github.com>
2025-03-28 16:41:30 +0000

    ADD baseline for dummy apps

---
+++ AzureFunction/helloGitTogether/index.js
@@ -0,0 +1,13 @@
+﻿module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hi, " + name + ". ¡Bienvjujjenidos a la demo de GitTogether!"
+        : "¡Welcome a la demo de GitTogether!";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
